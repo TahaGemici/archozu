cf70513007ea088895c7683927990d3d86d97cc7
kutaybulun <kutay.bulun@ozu.edu.tr>
2024-06-13 21:11:25 +0300

    sample uart to test added

---
+++ logic_design/Testing/uart_uvm/uart_uvm_tb.sv
@@ -0,0 +1,7 @@
+//TRANSACTION CYCLE:
+//1. Start new transaction
+//2. Apply control signals
+//3. Start transmitter and receiver
+//4. Wait for transaction to complete (tx_done and rx_done)
+//5. Complete transaction
+//6. Repeat
