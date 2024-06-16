eb455927d27e4a28716b10d83c53e5d37e2bf512
kutaybulun <kutay.bulun@ozu.edu.tr>
2024-06-16 23:21:47 +0300

    sco, env, agent and test added need DUT for rest

--- logic_design/Testing/uart_uvm/uart_uvm_tb.sv
+++ logic_design/Testing/uart_uvm/uart_uvm_tb.sv
@@ -21,13 +21,15 @@
 
 //VIRTUAL INTERFACE, UVM DONE ACCORDINGLY
 /* interface uart_if; 
- logic clk, rst;
- logic tx_start, rx_start;
- logic [7:0] tx_data;
- logic [16:0] baud_rate;
- logic [1:0] stop_bits;
- logic tx_done,rx_done;
- logic [7:0] rx_out;   
+logic clk, rst;
+logic tx_start, rx_start;
+logic [7:0] tx_data_i;
+logic [7:0] tx_data_o;
+logic [16:0] baud_rate;
+logic [1:0] stop_bits;
+logic tx_done,rx_done;
+logic [7:0] rx_data_i;
+logic [7:0] rx_data_o;   
 endinterface */
 
 `include "uvm_macros.svh"
@@ -56,8 +58,10 @@ class uart_transaction extends uvm_sequence_item;
     logic rst;
     logic tx_start, tx_done;
     logic rx_start, rx_done;
-    rand logic [7:0] tx_data;
-    logic [7:0] rx_out;
+    rand logic [7:0] tx_data_i;
+    logic [7:0] tx_data_o;
+    rand logic [7:0] rx_data_i;
+    logic [7:0] rx_data_o;
     rand logic [15:0] baud_rate;
     logic [1:0] stop_bits; //1, 1.5 or 2 stop bits , 00, 01, 1X respectively
     constraint baud_rate_c { baud_rate inside {4800, 9600, 14400, 19200, 38400, 57600}; }
@@ -85,7 +89,6 @@ class random_baud_1_stop extends uvm_sequence#(uart_transaction);
                 tr.rst = 1'b0;
                 tr.tx_start = 1'b1;
                 tr.rx_start = 1'b1;
-                tr.data_length = 8;
                 tr.stop_bits = 2'b00;
             finish_item(tr);
         end
@@ -111,7 +114,6 @@ class random_baud_1p5_stop extends uvm_sequence#(uart_transaction);
                 tr.rst = 1'b0;
                 tr.tx_start = 1'b1;
                 tr.rx_start = 1'b1;
-                tr.data_length = 8;
                 tr.stop_bits = 2'b01;
             finish_item(tr);
         end
@@ -137,7 +139,6 @@ class random_baud_2_stop extends uvm_sequence#(uart_transaction);
                 tr.rst = 1'b0;
                 tr.tx_start = 1'b1;
                 tr.rx_start = 1'b1;
-                tr.data_length = 8;
                 tr.stop_bits = 2'b10;
             finish_item(tr);
         end
@@ -166,7 +167,8 @@ class uart_driver extends uvm_driver#(uart_transaction);
             vif.rst <= 1'b1;
             vif.tx_start <= 1'b0;
             vif.rx_start <= 1'b0;
-            vif.tx_data <= 8'b0;
+            vif.tx_data_i <= 8'b0;
+            vif.rx_data_i <= 8'b0;
             vif.baud_rate <= 16'b0;
             vif.stop_bits <= 2'b00;
             `uvm_info("DRIVER", "Start of Simulation: Resetting DUT", UVM_NONE);
@@ -181,13 +183,15 @@ class uart_driver extends uvm_driver#(uart_transaction);
                 vif.rst <= 1'b0;
                 vif.tx_start <= tr.tx_start;
                 vif.rx_start <= tr.rx_start;
-                vif.tx_data <= tr.tx_data;
+                vif.tx_data_i <= tr.tx_data_i;
+                vif.rx_data_i <= tr.rx_data_i;
                 vif.baud_rate <= tr.baud_rate;
                 vif.stop_bits <= tr.stop_bits;
-                `uvm_info("DRIVER", $sformatf("BAUD:%0d STOP:%0d TX_DATA:%0d", tr.baud_rate, tr.stop_bits, tr.tx_data), UVM_NONE);
+                `uvm_info("DRIVER", $sformatf("BAUD:%0d STOP:%0d TX_DATA_IN:%0d RX_DATA_IN:%0d",
+                 tr.baud_rate, tr.stop_bits, tr.tx_data_i, tr.rx_data_i), UVM_NONE);
                 @(posedge vif.clk); //wait for the new clock cycle and tx_done and rx_done to drive new transaction
                 @(posedge vif.tx_done);
-                @(negedge vif.rx_done);
+                @(posedge vif.rx_done);
             seq_item_port.item_done(tr);
         end
     endtask
@@ -228,13 +232,18 @@ class uart_monitor extends uvm_monitor;
                 @(posedge vif.tx_done); //wait for tx_done to sample the data from uart_tx
                 tr.rst = 1'b0;
                 tr.tx_start = vif.tx_start;
-                tr.rx_start = vif.rx_start;
-                tr.tx_data = vif.tx_data;
+                tr.tx_data_i = vif.tx_data_i;
+                tr.tx_data_o = vif.tx_data_o;
                 tr.baud_rate = vif.baud_rate;
                 tr.stop_bits = vif.stop_bits;
-                @(negedge vif.rx_done); //wait for rx_done to sample the data from uart_rx
-                tr.rx_out = vif.rx_out;
-                `uvm_info("MONITOR", $sformatf("BAUD:%0d STOP:%0d TX_DATA:%0d RX_DATA:%0d", tr.baud_rate, tr.stop_bits, tr.tx_data, tr.rx_out), UVM_NONE);
+                tr.tx_done = vif.tx_done;
+                @(posedge vif.rx_done); //wait for rx_done to sample the data from uart_rx
+                tr.rx_start = vif.rx_start;
+                tr.rx_data_i = vif.rx_data_i;
+                tr.rx_data_o = vif.rx_data_o;
+                tr.rx_done = vif.rx_done;
+                `uvm_info("MONITOR", $sformatf("BAUD:%0d STOP:%0d TX_DATA_OUT:%0d RX_DATA_OUT:%0d",
+                 tr.baud_rate, tr.stop_bits, tr.tx_data_o, tr.rx_data_o), UVM_NONE);
                 send.write(tr);
             end
         end
@@ -242,5 +251,137 @@ class uart_monitor extends uvm_monitor;
 
     virtual task run_phase(uvm_phase phase);
         monitor();
+    endtask
+endclass
+
+//UART SCOREBOARD
+//TX and RX are not connected to each other, they are independent
+//Very trivial scoreboard, just checking if the data is transmitted and received correctly
+//Might need to add a counter to check if the design works at the correct baud rate
+class uart_scoreboard extends uvm_scoreboard;
+    `uvm_component_utils(uart_scoreboard)
+    uvm_analysis_imp#(uart_transaction, uart_scoreboard) recv;
+
+    function new(input string name = "uart_scoreboard", uvm_component parent = null);
+        super.new(name, parent);
+    endfunction
+
+    virtual function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        recv = new("recv", this);
+    endfunction
+
+    virtual function void write(uart_transaction tr);
+        `uvm_info("SCOREBOARD", $sformatf("BAUD:%0d STOP:%0d TX_DATA_IN:%0d TX_DATA_OUT:%0d RX_DATA_IN:%0d RX_DATA_OUT:%0d",
+         tr.baud_rate, tr.stop_bits, tr.tx_data_i, tr.tx_data_o, tr.rx_data_i, tr.rx_data_o), UVM_NONE);
+        if(tr.rst == 1) begin
+            `uvm_info("SCOREBOARD", "System Reset Detected", UVM_NONE);
+        end
+        else begin
+            if(tr.tx_done == 1) begin
+                `uvm_info("SCOREBOARD", "TX Done Detected", UVM_NONE);
+                if(tr.tx_data_i == tr.tx.data_o) `uvm_info("SCOREBOARD", "TX Data Match", UVM_NONE);
+                else `uvm_error("SCOREBOARD", "TX Data Mismatch");
+            end
+            else begin
+                `uvm_error("SCOREBOARD", "TX Done Not Detected");
+            end
+            if(tr.rx_done == 1) begin
+                `uvm_info("SCOREBOARD", "RX Done Detected", UVM_NONE);
+                if(tr.rx_data_i == tr.rx_data_o) `uvm_info("SCOREBOARD", "RX Data Match", UVM_NONE);
+                else `uvm_error("SCOREBOARD", "RX Data Mismatch");
+            end
+            else begin
+                `uvm_error("SCOREBOARD", "RX Done Not Detected");
+            end
+        end
+    endfunction
+endclass
+
+//UART AGENT
+class uart_agent extends uvm_agent;
+    `uvm_component_utils(uart_agent)
+    uart_config cfg;
+
+    function new(input string name = "uart_agent", uvm_component parent = null);
+        super.new(name, parent);
+    endfunction
+
+    uart_driver drv;
+    uart_monitor mon;
+    uvm_sequencer#(uart_transaction) seqr;
+
+    virtual function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        cfg = uart_config::type_id::create("cfg");
+        mon = uart_monitor::type_id::create("mon", this);
+        if(cfg.is_active == UVM_ACTIVE) begin
+            drv = uart_driver::type_id::create("drv", this);
+            seqr = uvm_sequencer#(uart_transaction)::type_id::create("seqr", this);
+        end
+    endfunction
+
+    virtual function void connect_phase(uvm_phase phase);
+        super.connect_phase(phase);
+        if(cfg.is_active == UVM_ACTIVE) begin
+            drv.seq_item_port.connect(seqr.seq_item_export);
+        end
+    endfunction
+endclass
+
+//UART ENVIRONMENT
+class uart_env extends uvm_env;
+    `uvm_component_utils(uart_env)
+
+    function new(input string name = "uart_env", uvm_component c);
+        super.new(name, c);
+    endfunction
+
+    uart_agent agt;
+    uart_scoreboard sb;
+
+    virtual function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        agt = uart_agent::type_id::create("agt", this);
+        sb = uart_scoreboard::type_id::create("sb", this);
+    endfunction
+
+    virtual function void connect_phase(uvm_phase phase);
+        super.connect_phase(phase);
+        agt.mon.send.connect(sb.recv);
+    endfunction
+endclass
+
+//UART TEST
+class uart_test extends uvm_test;
+    `uvm_component_utils(uart_test)
+
+    function new(input string name = "uart_test", uvm_component c);
+        super.new(name, c);
+    endfunction
+
+    uart_env env;
+    random_baud_1_stop seq1;
+    random_baud_1p5_stop seq2;
+    random_baud_2_stop seq3;
+
+    virtual function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        env = uart_env::type_id::create("env", this);
+        seq1 = random_baud_1_stop::type_id::create("seq1", this);
+        seq2 = random_baud_1p5_stop::type_id::create("seq2", this);
+        seq3 = random_baud_2_stop::type_id::create("seq3", this);
+    endfunction
+
+    virtual task run_phase(uvm_phase phase);
+        super.run_phase(phase);
+        phase.raise_objection(this);
+            seq1.start(env.agt.seqr);
+            #100;
+            seq2.start(env.agt.seqr);
+            #100;
+            seq3.start(env.agt.seqr);
+            #100;
+        phase.drop_objection(this);
     endtask
 endclass
