5128d4f7d190935be6fe3eaa12697a076ecd185e
Taha <118563016+TahaGemici@users.noreply.github.com>
2024-07-08 12:52:24 +0300

    .

--- RTL/bus/cv32e40p/cv32e40p_clock_gate.v
+++ RTL/bus/cv32e40p/cv32e40p_clock_gate.v
@@ -4,9 +4,17 @@ module cv32e40p_clock_gate(
     input scan_cg_en_i,
     output clk_o
 );
-    BUFGCE BUFGCE_inst (
-        .O(clk_o),
-        .CE(en_i | scan_cg_en_i),
-        .I(clk_i)
-    );
+    `ifdef FPGA
+        BUFGCE BUFGCE_inst (
+            .O(clk_o),
+            .CE(en_i | scan_cg_en_i),
+            .I(clk_i)
+        );
+    `else
+        reg en_q;
+        always @(negedge clk_i) begin
+           en_q <= en_i | scan_cg_en_i;
+        end
+        assign clk_o = clk_i & en_q;
+    `endif
 endmodule
