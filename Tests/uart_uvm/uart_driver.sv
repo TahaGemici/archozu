b10b0fff5a672ee1811fcc7588ff7ef51d4457de
kutaybulun <kutay.bulun@ozu.edu.tr>
2024-08-27 22:45:22 +0300

    uart driver updated

--- Tests/uart_uvm/uart_driver.sv
+++ Tests/uart_uvm/uart_driver.sv
@@ -10,6 +10,10 @@
 
     virtual uart_intf vif;
     reg [7:0] data;
+    reg [7:0] captured_tx_data;
+    reg tx_en;
+    logic rx_done;
+    logic tx_done;
     int no_transactions;
     
     
@@ -22,39 +26,42 @@
       if( !uvm_config_db #(virtual uart_intf)::get(this, "", "uart_intf", vif) )
         `uvm_error("", "uvm_config_db::get failed")
     endfunction 
-   
+
     task run_phase(uvm_phase phase);
       forever
       begin
         seq_item_port.get_next_item(req);
-
-        
       `uvm_info("","---------------------------------------------",UVM_MEDIUM) 
       `uvm_info("", $sformatf("\t Transaction No. = %0d",no_transactions),UVM_MEDIUM) 
       //Test tx
       //vif.start <= 1;
-      vif.cfg_i[0] <= 1;
+      vif.cfg_i <= 1;
       vif.rx_i <= 1;
       vif.cbp_i <= clock_divide;
       vif.stop_bits_i <= 0;
-      vif.write_i <= 1;
-      @(posedge vif.clk_i);
-      vif.tx_data_i <= req.tx_data_i;
       vif.write_i <= 0;
       @(posedge vif.clk_i);
-      wait(vif.done_tx_o == 1);
-      //vif.start <= 0;
-      vif.cfg_i[0] <= 0;
-      if(vif.done_tx_o == 1) begin
-      `uvm_info("", $sformatf("\t cfg_i = %0b, \t tx_data_i = %0h,\t done_tx_o = %0b",vif.cfg_i,req.tx_data_i,vif.done_tx_o),UVM_MEDIUM)  
-      `uvm_info("","[TRANSACTION]::TX PASS",UVM_MEDIUM)  
-       end
-      else begin
-      `uvm_info("", $sformatf("\t cfg_i = %0b, \t tx_data_i = %0h,\t done_tx_o = %0b",vif.cfg_i,req.tx_data_i,vif.done_tx_o),UVM_MEDIUM)  
-      `uvm_info("","[TRANSACTION]::TX FAIL",UVM_MEDIUM)  
-       end  
-       vif.cfg_i[2] <= 0;
+      @(posedge vif.clk_i);
+      vif.tx_data_i <= req.tx_data_i;
+      vif.write_i <= 1;
+      repeat(clock_divide) @(posedge vif.clk_i);
+      
+      for(int i=0;i<8;i++) begin
+        repeat(clock_divide) @(posedge vif.clk_i);
+        captured_tx_data[i] <= vif.tx_o;
+        //repeat(clock_divide) @(posedge vif.clk_i);
+      end
+
+      repeat(clock_divide*2) @(posedge vif.clk_i);
+        vif.cfg_i <= 0;
+        if (captured_tx_data !== vif.tx_data_i) begin
+            `uvm_error("TX MISMATCH", $sformatf("Mismatch: Expected %8b, Got %8b on tx_o", vif.tx_data_i, captured_tx_data));
+        end else begin
+            `uvm_info("TX MATCH", $sformatf("Match: Expected %8b, Got %8b on tx_o", vif.tx_data_i, captured_tx_data), UVM_MEDIUM);
+        end
+
       repeat(100) @(posedge vif.clk_i);
+
       //Test rx
 	    @(posedge vif.clk_i);
 	    data = $random;
@@ -68,9 +75,10 @@
 	    end
 	    vif.rx_i <= 1'b1;
 	    repeat(clock_divide) @(posedge vif.clk_i);
-      vif.cfg_i[1] <= 0;
+        vif.cfg_i[1] <= 0;
+
 	    repeat(100) @(posedge vif.clk_i); 
-	   `uvm_info("", $sformatf("\t Expected data = %0h, \t Obtained data = %0h", data,vif.rx_data_o),UVM_MEDIUM)  
+	   `uvm_info("", $sformatf("\t Expected data = %h, \t Obtained data = %h", data,vif.rx_data_o),UVM_MEDIUM)  
       begin
 	    if(vif.rx_data_o == data) begin
 	    `uvm_info("","[TRANSACTION]::RX PASS",UVM_MEDIUM)  
