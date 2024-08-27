7c18f740c81cac077bf44bca68c3bd8c883d10f2
kutaybulun <kutay.bulun@ozu.edu.tr>
2024-08-27 22:54:17 +0300

    coverage report

--- Tests/uart_uvm/uart_cov.sv
+++ Tests/uart_uvm/uart_cov.sv
@@ -3,21 +3,20 @@ class uart_cov extends uvm_subscriber #(uart_trans);
   
   `uvm_component_utils(uart_cov)
   uart_trans trans;
-	
 
   covergroup cov_inst;
   RX:coverpoint trans.rx_i {option.auto_bin_max = 1;}
-  TX_DIN:coverpoint trans.tx_data_i {option.auto_bin_max = 8;}
+  TX_DIN:coverpoint trans.tx_data_i {option.auto_bin_max = 4;}
   TX_START:coverpoint trans.cfg_i[0] {option.auto_bin_max = 1;}
   TX:coverpoint trans.tx_o {option.auto_bin_max = 1;}
-  RX_DOUT:coverpoint trans.rx_data_o {option.auto_bin_max = 8;}
+  RX_DOUT:coverpoint trans.rx_data_o {option.auto_bin_max = 4;}
   //TX_ACT:coverpoint trans.tx_active {option.auto_bin_max = 1;}
   TX_DONE:coverpoint trans.done_tx_o {option.auto_bin_max = 1;}
   RX_DONE:coverpoint trans.done_rx_o {option.auto_bin_max = 1;}
 
-  RXxRX_DOUTxRX_DONE: cross RX,RX_DOUT,RX_DONE;
-  TXxTX_DINxTX_DONE: cross TX,TX_DIN,TX_DONE;
-  TX_STARTxTX_DIN: cross TX_START,TX_DIN;
+  //RXxRX_DOUTxRX_DONE: cross RX,RX_DOUT,RX_DONE;
+  //TXxTX_DINxTX_DONE: cross TX,TX_DIN,TX_DONE;
+  //TX_STARTxTX_DIN: cross TX_START,TX_DIN;
   endgroup 
   
   
